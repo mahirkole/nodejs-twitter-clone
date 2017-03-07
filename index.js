9b6f3e48e56542ae75acd50aed1b87c57f9558a0
Mahir Köle <mahirkole@gmail.com>
2017-03-08 01:18:10 +0300

    app_list.js added, so that applications can be accessed one by one.

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ var config = require('./config');
 var database = require('./database');
 var fs = require('fs');
 var path = require('path');
+var app_list = require('./src/applications/app_list');
 
-
+app_list.forEach(_app => {
+  var router = require(path.join(
+    __dirname, 'src', 'applications', _app.folder, _app.route));
+  app.use(_app.url, router);
+});
 
 app.get('/', function(req, res, next){
   res.send("OK!", 200);
---
+++ src/applications/app_list.js
@@ -0,0 +1,7 @@
+module.exports = [{
+  folder: 'profile',
+  route: 'profile.route.js',
+  controller: 'profile.controller.js',
+  model: 'profile.model.js',
+  url: '/profile'
+}]
